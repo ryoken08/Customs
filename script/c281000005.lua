b004717132f72aabdddcd0168ed1e95a8203c702
ryoken08 <nekushiro@gmail.com>
2021-01-08 20:57:03 -0600

    Updated "Re:Zero Subaru" effect
    Now it gains a Counter for each time the card has been Special Summoned by it's own effect

--- script/c281000005.lua
+++ script/c281000005.lua
@@ -21,16 +21,27 @@ function s.initial_effect(c)
 	e2:SetCondition(s.spcon)
 	e2:SetTarget(s.sptg)
 	e2:SetOperation(s.spop)
+	e2:SetLabel(0)
 	c:RegisterEffect(e2)
-	--Place counters
+	--Place counters (special summon)
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,1))
-	e3:SetType(EFFECT_TYPE_IGNITION)
-	e3:SetCountLimit(1,id)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e3:SetRange(LOCATION_MZONE)
-	e3:SetTarget(s.cttg)
-	e3:SetOperation(s.ctop)
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e3:SetCondition(s.ctcon1)
+	e3:SetOperation(s.ctop1)
+	e3:SetLabelObject(e2)
 	c:RegisterEffect(e3)
+	--Place counters (field)
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,1))
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetCountLimit(1,id)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetTarget(s.cttg2)
+	e4:SetOperation(s.ctop2)
+	c:RegisterEffect(e4)
 end
 s.counter_place_list={COUNTER_WITCH}
 --reborn
@@ -48,16 +59,25 @@ end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) then
-		if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
-		   e:GetHandler():AddCounter(COUNTER_WITCH,1) 
-		end
+		Duel.SpecialSummon(c,1,tp,tp,false,false,POS_FACEUP)
+		e:SetLabel(e:GetLabel()+1)
 	end
 end
---counter
-function s.cttg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+--Place counters (special summon)
+function s.ctcon1(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetSummonType()==SUMMON_TYPE_SPECIAL+1
+end
+function s.ctop1(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsFaceup() and c:IsRelateToEffect(e) then
+		e:GetHandler():AddCounter(COUNTER_WITCH,e:GetLabelObject():GetLabel())
+	end
+end
+--Place counters (field)
+function s.cttg2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chk==0 then return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsType,TYPE_MONSTER),tp,LOCATION_MZONE,LOCATION_MZONE,1,e:GetHandler()) end
 end
-function s.ctop(e,tp,eg,ep,ev,re,r,rp)
+function s.ctop2(e,tp,eg,ep,ev,re,r,rp)
 	local ct=Duel.GetMatchingGroupCount(aux.FilterFaceupFunction(Card.IsType,TYPE_MONSTER),tp,LOCATION_MZONE,LOCATION_MZONE,e:GetHandler())
 	e:GetHandler():AddCounter(COUNTER_WITCH,ct)
 end
